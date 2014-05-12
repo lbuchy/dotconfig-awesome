d7b4de65f60e994f72a16d012c400b4ca9bfae3a
Logan Buchy <logan.buchy@gmail.com>
2014-05-12 00:59:32 -0700

    Make sakura default terminal

--- settings/editor.lua
+++ settings/editor.lua
@@ -1,4 +1,4 @@
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "sakura"
 editor = os.getenv("EDITOR") or "subl"
 editor_cmd = terminal .. " " .. editor
